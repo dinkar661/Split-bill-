0b0bff629569e60c7aa3d50a9394c090f6383c7d
dinkar661 <dinkarkumar661@gmail.com>
2025-08-31 11:16:38 +0530

    Add split bill project

---
+++ index.js
@@ -0,0 +1,16 @@
+function calculateSplit() {
+  const bill = parseFloat(document.getElementById('bill').value);
+  const tip = parseFloat(document.getElementById('tip').value || 0);
+  const people = parseInt(document.getElementById('people').value);
+
+    if ((bill<=0)||(people)<=0) {
+    document.getElementById("result").innerHTML = "Please enter valid inputs.";
+    return;
+  }
+
+  const tipAmount =(tip/100)*bill
+  const total = bill+tipAmount
+  const perPerson = total/people
+
+  document.getElementById("result").textContent = `Each person pays: ₹${perPerson.toFixed(2)}`;
+}
